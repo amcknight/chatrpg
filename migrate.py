2a4bb69f8609cb7d616d28c395d48d448ba4f9c5
Andrew McKnight <thedonk@gmail.com>
2023-01-30 15:39:33 -0800

    migrate again

--- migrate.py
+++ migrate.py
@@ -2,8 +2,7 @@ import redis
 
 def move_xp():
     r = redis.Redis(host='localhost', port=6379, db=0)
-    keys = list( map( lambda k: k.decode(), r.keys('*:xp:*') ) )
-    dirty_keys = list(filter(dirty_key, keys))
+    dirty_keys = list( map( lambda k: k.decode(), r.keys("*:xp:b'*'") ) )
     clean_keys = []
     for key in dirty_keys:
         key_parts = key.split(':')
@@ -11,25 +10,24 @@ def move_xp():
     
     print(f"{dirty_keys, clean_keys}")
 
+# def dirty_key(key):
+#     key_parts = key.split(':')
+#     if any(map(dirty_key_part, key_parts)):
+#         return True
+#     return False
+
+# def dirty_key_part(key_part):
+#     try:
+#         key_part
+#         return True
+#     except (UnicodeDecodeError, AttributeError):
+#         return False
+
 def safe_decode(bytes_or_str):
     try:
         return bytes_or_str.decode()
     except (UnicodeDecodeError, AttributeError):
         return bytes_or_str
-
-def dirty_key_part(key_part):
-    try:
-        key_part.decode()
-        return True
-    except (UnicodeDecodeError, AttributeError):
-        return False
-
-def dirty_key(key):
-    key_parts = key.split(':')
-    if any(map(dirty_key_part, key_parts)):
-        return True
-    return False
-
 
 if __name__ == '__main__':
     move_xp()
