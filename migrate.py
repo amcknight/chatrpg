a78c52a516eb0fe7ae2964477e1b701d82cc8fda
Andrew McKnight <thedonk@gmail.com>
2023-01-30 16:01:21 -0800

    neutralizing migrate so only works with manual mods

--- migrate.py
+++ migrate.py
@@ -4,11 +4,10 @@ def move_xp():
     r = redis.Redis(host='localhost', port=6379, db=0)
     dirty_keys = list( map( lambda k: k.decode(), r.keys("*:xp:b'*'") ) )
     clean_keys = list( map( clean_key, dirty_keys ) )
-    
+
     for (dk, ck) in zip(dirty_keys, clean_keys):
         r.incrby(ck, r.get(dk))
         r.delete(dk)
-    print(f"{dirty_keys, clean_keys}")
 
 def clean_key(key):
     key_part = key.split(':')
@@ -18,4 +17,5 @@ def clean_key(key):
     return f"{key_prefix}:{clean_part}"
 
 if __name__ == '__main__':
-    move_xp()
+    pass
+    #move_xp()
